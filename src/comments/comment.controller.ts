f71c0d4f9c2608418407edd93ed974a6872256f7
Tomzy <tomiwadavid10@gmail.com>
2025-08-14 17:06:25 +0100

    comments

---
+++ src/comments/comment.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Post, Get, Param, Body,Put, Delete } from '@nestjs/common';
+import { CommentService } from './comment.service';
+
+@Controller('tasks/:taskId/comments')
+export class CommentController {
+  constructor(private readonly commentService: CommentService) {}
+
+  @Post()
+  addComment(
+    @Param('taskId') taskId: string,
+    @Body() body: { content: string; userId: string },
+  ) {
+    return this.commentService.addComment(taskId, body.content, body.userId);
+  }
+
+  @Put(':commentId')
+  editComment(
+    @Param('taskId') taskId: string,
+    @Param('commentId') commentId: string,
+    @Body() body: { content: string },
+  ) {
+    return this.commentService.editComment(commentId, body.content);
+  }
+
+   @Delete(':commentId')
+  deleteComment(
+    @Param('taskId') taskId: string,
+    @Param('commentId') commentId: string,
+  ) {
+    return this.commentService.deleteComment(commentId);
+  }
+
+  @Get()
+  getComments(@Param('taskId') taskId: string) {
+    return this.commentService.getComments(taskId);
+  }
+}
---
+++ src/comments/comment.entity.ts
@@ -0,0 +1,29 @@
+import { Entity,PrimaryGeneratedColumn,Column,ManyToOne,CreateDateColumn, UpdateDateColumn} from 'typeorm';
+import { Task } from '../tasks/entities/task.entity';
+import { User } from '../user/user.entity';
+import { CalendarTask } from '../entities/calendartask.entity';
+
+@Entity()
+export class Comment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  content: string;
+
+  @ManyToOne(() => Task, (task: Task) => task.comments)
+  task: Task;
+
+  @ManyToOne(() => User, user => user.comments, { onDelete: 'CASCADE' })
+  user: User;
+
+  @ManyToOne(() => CalendarTask, calendarTask => calendarTask.comments)
+  calendarTask: CalendarTask;
+
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
