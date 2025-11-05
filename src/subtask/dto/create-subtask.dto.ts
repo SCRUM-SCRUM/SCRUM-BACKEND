acf6a1671aa589fefbeb4f26070c4b433a3ed74a
Peemozi <praisejesulonimi@gmail.com>
2025-11-05 12:00:09 +0100

    all recent updates

--- src/subtask/dto/create-subtask.dto.ts
+++ src/subtask/dto/create-subtask.dto.ts
@@ -1,6 +1,12 @@
 /* eslint-disable prettier/prettier */
+import { IsMongoId, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateSubtaskDto {
+  @IsNotEmpty()
+  @IsMongoId()
+  taskId: number;
+
+  @IsNotEmpty()
+  @IsString()
   title: string;
-   isDone?: boolean;
-  taskId?: number;
 }
